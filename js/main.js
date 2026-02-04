553b716a76e963a523cd299dee357df474784e46
siazur <asiazur@yandex.com>
2026-02-04 23:47:08 +0300

    todo

---
+++ js/main.js
@@ -0,0 +1,59 @@
+//поиск элементов на странице
+const taskInput = document.querySelector('#taskInput'); 
+const form = document.querySelector('#form');
+const tasksList = document.querySelector('#tasksList');
+
+//вызов функции addTask при отправке
+form.addEventListener('submit', addTask); 
+
+//вызов функции удаления задачи при клике на крестик
+tasksList.addEventListener('click', deleteTask);
+
+//вызов функции выполненной задачи при клике на окошко
+tasksList.addEventListener('click', doneButton);
+
+//функция добавления задачи в список
+function addTask(event) { 
+
+    //отмена перезагрузки страницы
+    event.preventDefault(); 
+
+    //значение из ввода
+    const taskText = taskInput.value;
+
+    const taskHTML = `<li class="task">
+                        <div class="task-elements">
+                            <button class="task-button" id="task-button">
+                                <span><img src="/img/check1.svg" alt="" class="task-img"></span>
+                            </button>
+                            <span class="task-text">${taskText}</span>
+                        </div>
+                        <button class="delete-button">✕</button>
+                    </li>`;
+
+    //добавляем новый элемент в конец списка tasksList
+    tasksList.insertAdjacentHTML('beforeend', taskHTML);
+    
+    //очистка поля ввода
+    taskInput.value = "";
+}
+
+//выполненная задача
+function doneButton (event) {
+    const doneButton = event.target.closest('.task-button');
+    if (doneButton) {
+        const img = event.target.closest('.task-button').querySelector('.task-img');
+        img.src = img.src.includes('check1.svg') ? '/img/check.svg' : '/img/check1.svg';
+    }
+}
+
+//удаление задачи
+function deleteTask (event) {
+    const deleteButton = event.target.closest('.delete-button');
+    if (deleteButton) {
+        const taskItem = event.target.closest('.task');
+        taskItem.remove();
+    }
+}
+
+
