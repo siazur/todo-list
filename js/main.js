0e2fc8630746d5f591346677538c0ec8b57e02bf
siazur <asiazur@yandex.com>
2026-02-15 21:43:50 +0300

    Добавлен localstorage и перенос строк в css

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ const taskInput = document.querySelector('#taskInput');
 const form = document.querySelector('#form');
 const tasksList = document.querySelector('#tasksList');
 
+let tasks = [];
+
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+}
+
+tasks.forEach(function(task) {
+    renderTask(task);
+});
+
 //вызов функции addTask при отправке
 form.addEventListener('submit', addTask); 
 
@@ -21,18 +31,18 @@ function addTask(event) {
     //значение из ввода
     const taskText = taskInput.value;
 
-    const taskHTML = `<li class="task">
-                        <div class="task-elements">
-                            <button class="task-button" id="task-button">
-                                <span><img src="./img/checknull.svg" alt="" class="task-img"></span>
-                            </button>
-                            <span class="task-text">${taskText}</span>
-                        </div>
-                        <button class="delete-button">✕</button>
-                    </li>`;
+    //объект описание каждой задачи
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        complete: false,
+    }
 
-    //добавляем новый элемент в конец списка tasksList
-    tasksList.insertAdjacentHTML('beforeend', taskHTML);
+    tasks.push(newTask);
+
+    saveTask();
+
+    renderTask(newTask);
     
     //очистка поля ввода
     taskInput.value = "";
@@ -43,17 +53,63 @@ function doneButton (event) {
     const doneButton = event.target.closest('.task-button');
     if (doneButton) {
         const img = event.target.closest('.task-button').querySelector('.task-img');
-        img.src = img.src.includes('checknull.svg') ? './img/check.svg' : './img/check1.svg';
+
+        const taskItem = event.target.closest('.task');
+        const id = Number(taskItem.id);
+
+        const task = tasks.find(function(task) {
+            if (task.id == id) {
+                return true;
+            }
+        })
+        task.complete = !task.complete;
+
+        saveTask();
+
+        img.src = img.src.includes('checknull.svg') ? './img/check.svg' : './img/checknull.svg';
     }
 }
 
 //удаление задачи
 function deleteTask (event) {
     const deleteButton = event.target.closest('.delete-button');
+
     if (deleteButton) {
         const taskItem = event.target.closest('.task');
+
+        //id удаляемого элемента 
+        const id = Number(taskItem.id);
+
+        //находим его в массиве по id 
+        const index = tasks.findIndex(function(task) {
+            return task.id === id;
+        })
+
+        tasks.splice(index, 1);
+        saveTask();
+
         taskItem.remove();
     }
 }
 
+function saveTask() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function renderTask(task) {
+    imgSrc = task.complete ? './img/check.svg' : './img/checknull.svg';
+
+    const taskHTML = `<li id="${task.id}" class="task">
+                        <div class="task-elements">
+                            <button class="task-button" id="task-button">
+                                <span><img src="${imgSrc}" alt="" class="task-img"></span>
+                            </button>
+                            <span class="task-text">${task.text}</span>
+                        </div>
+                        <button class="delete-button">✕</button>
+                    </li>`;
+
+    //добавляем новый элемент в конец списка tasksList
+    tasksList.insertAdjacentHTML('beforeend', taskHTML);
+}
 
